a114eabb4c85624c6a1184dccc2d332f72f9db2a
Dreadhalor <Scotthetrick2@yahoo.com>
2024-04-13 20:21:48 -0700

    Implemented browse-action-bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,59 @@
-export default function Home() {
+import { BrowseActionBarButton } from "@/components/browse-action-bar-button";
+import { MdUpload } from "react-icons/md";
+import { FiFolderPlus } from "react-icons/fi";
+import {
+  MdOutlineDriveFolderUpload,
+  MdOutlineUploadFile,
+} from "react-icons/md";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@ui/dropdown-menu";
+
+const Page = () => {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      If you can&apos;t tell the difference between a spoon and a ladle, then
-      you&apos;re fat
-    </main>
+    <div className="flex h-full border-4 border-blue-600">
+      <div className="w-[240px] border-4">SIDEBAR</div>
+      <div className="flex-1 flex-col border-4">
+        <div className="flex gap-[20px] border border-blue-500 p-2">
+          <DropdownMenu modal={false}>
+            <DropdownMenuTrigger asChild>
+              <BrowseActionBarButton primary>
+                <>
+                  <MdUpload size={24} />
+                  Upload
+                </>
+              </BrowseActionBarButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="start">
+              <DropdownMenuItem className="cursor-pointer gap-2">
+                <>
+                  <MdOutlineUploadFile size={16} />
+                  File
+                </>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer gap-2">
+                <>
+                  <MdOutlineDriveFolderUpload size={16} />
+                  Folder
+                </>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <BrowseActionBarButton>
+            <>
+              <FiFolderPlus size={24} />
+              Create folder
+            </>
+          </BrowseActionBarButton>
+        </div>
+        MAIN CONTENT
+      </div>
+    </div>
   );
-}
+};
+
+export default Page;
