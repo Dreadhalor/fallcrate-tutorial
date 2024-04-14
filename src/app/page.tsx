efdfd2c0509843e6312e2b1358d08c36c537563e
Dreadhalor <Scotthetrick2@yahoo.com>
2024-04-13 20:41:42 -0700

    Abstracted out browse-action-bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,11 @@
-import { BrowseActionBarButton } from "@/components/browse-action-bar-button";
-import { MdUpload } from "react-icons/md";
-import { FiFolderPlus } from "react-icons/fi";
-import {
-  MdOutlineDriveFolderUpload,
-  MdOutlineUploadFile,
-} from "react-icons/md";
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@ui/dropdown-menu";
+import { BrowseActionBar } from "@/components/browse-action-bar";
 
 const Page = () => {
   return (
     <div className="flex h-full border-4 border-blue-600">
       <div className="w-[240px] border-4">SIDEBAR</div>
       <div className="flex-1 flex-col border-4">
-        <div className="flex gap-[20px] border border-blue-500 p-2">
-          <DropdownMenu modal={false}>
-            <DropdownMenuTrigger asChild>
-              <BrowseActionBarButton primary>
-                <>
-                  <MdUpload size={24} />
-                  Upload
-                </>
-              </BrowseActionBarButton>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="start">
-              <DropdownMenuItem className="cursor-pointer gap-2">
-                <>
-                  <MdOutlineUploadFile size={16} />
-                  File
-                </>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="cursor-pointer gap-2">
-                <>
-                  <MdOutlineDriveFolderUpload size={16} />
-                  Folder
-                </>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <BrowseActionBarButton>
-            <>
-              <FiFolderPlus size={24} />
-              Create folder
-            </>
-          </BrowseActionBarButton>
-        </div>
+        <BrowseActionBar />
         MAIN CONTENT
       </div>
     </div>
---
+++ src/components/browse-action-bar.tsx
@@ -0,0 +1,51 @@
+import { BrowseActionBarButton } from "@/components/browse-action-bar-button";
+import { MdUpload } from "react-icons/md";
+import { FiFolderPlus } from "react-icons/fi";
+import {
+  MdOutlineDriveFolderUpload,
+  MdOutlineUploadFile,
+} from "react-icons/md";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@ui/dropdown-menu";
+
+export const BrowseActionBar = () => {
+  return (
+    <div className="flex gap-[20px] border border-blue-500 p-2">
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <BrowseActionBarButton primary>
+            <>
+              <MdUpload size={24} />
+              Upload
+            </>
+          </BrowseActionBarButton>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="start">
+          <DropdownMenuItem className="cursor-pointer gap-2">
+            <>
+              <MdOutlineUploadFile size={20} />
+              File
+            </>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="cursor-pointer gap-2">
+            <>
+              <MdOutlineDriveFolderUpload size={20} />
+              Folder
+            </>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <BrowseActionBarButton>
+        <>
+          <FiFolderPlus size={24} />
+          Create folder
+        </>
+      </BrowseActionBarButton>
+    </div>
+  );
+};
